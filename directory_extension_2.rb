350367ed770dbcaf630741d821ee5a2024e9bd9b
Dan Marshall <daniel.o.marshall@gmail.com>
2021-09-15 16:21:47 +0100

    Added add_student method

--- directory_extension_2.rb
+++ directory_extension_2.rb
@@ -40,7 +40,7 @@ def load_students(filename = "students.csv")
   file = File.open(filename, "r")
   file.readlines.each do |line|
   name, cohort = line.chomp.split(',')
-    @students << {name: name, cohort: cohort.to_sym}
+    add_student(name, cohort)
   end
   file.close
 end
@@ -55,6 +55,10 @@ def save_students
     file.puts csv_line
   end
   file.close
+end
+
+def add_student(name, cohort)
+  @students << {name: name, cohort: cohort.to_sym}
 end
 
 def print_menu
@@ -97,7 +101,7 @@ def input_students
   # while the name is not empty, repeat this code
   while !name.empty? do
     # add the student has to the array
-    @students << {name: name, cohort: :november}
+    add_student(name, :november)
     puts "Now we have #{@students.count} students"
 
     # get another name from the user
